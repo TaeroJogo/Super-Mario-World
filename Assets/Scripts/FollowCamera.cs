a0e34aa33825538b055014008e326eef34fc2082
LeomaiaJr <leomaiajr@hotmail.com>
2021-06-20 21:31:54 -0300

    adicionar camera seguir o mario

---
+++ Assets/Scripts/FollowCamera.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCamera : MonoBehaviour
+{
+
+    private Transform playerTransform;
+    void Start()
+    {
+        playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    void Update()
+    {
+        Vector3 temp = transform.position;
+        temp.x = playerTransform.position.x;
+
+        transform.position = temp;
+    }
+}
